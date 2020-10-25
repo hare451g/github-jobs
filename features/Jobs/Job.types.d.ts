8516bb23754eb631bdd82d60f4c51650d86efac4
hare <hendrasadewa30@gmail.com>
2020-10-25 10:57:08 +0700

    create job types

---
+++ features/Jobs/Job.types.d.ts
@@ -0,0 +1,41 @@
+interface Job {
+  id: string;
+  type: string;
+  url: string;
+  created_at: string;
+  company: string;
+  company_url: string;
+  location: string;
+  title: string;
+  description: string;
+  how_to_apply: string;
+  company_logo: string;
+}
+
+type requestParams = {
+  description?: string;
+  location?: string;
+  lat?: string;
+  long?: string;
+  fullTime?: string;
+};
+
+type jobStateTypes = {
+  loading: boolean;
+  error?: Error['message'];
+  list: listType;
+  ids: idsType;
+};
+
+type fetchPositionsType = (params: requestParams) => Promise<void>;
+type listType = { [key: string]: Job };
+type idsType = Array<Job['id']>;
+
+export {
+  Job,
+  jobStateTypes,
+  requestParams,
+  fetchPositionsType,
+  listType,
+  idsType,
+};
