fcfe8a23a512160d95c03b604d8ba61b6669c163
hare <hendrasadewa30@gmail.com>
2020-11-06 23:35:12 +0700

    add job details provider

--- features/Details/index.tsx
+++ features/Details/index.tsx
@@ -1,5 +1,27 @@
+import Link from 'next/link';
+import { useEffect } from 'react';
+import MaterialIcons from '../../components/MaterialIcons';
+import styles from './styles/index.module.css';
+import useJobDetails, { JobDetailsContext } from './useJobDetails';
+
 function Details({ id }) {
-  return <div>path: {id} </div>;
+  const jobDetailsFeature = useJobDetails();
+
+  useEffect(() => {
+    jobDetailsFeature.actions.fetchJobDetails(id);
+  }, [id]);
+
+  return (
+    <JobDetailsContext.Provider value={jobDetailsFeature}>
+      <h1>Github Jobs</h1>
+      <Link href="/">
+        <a className={styles.navBack}>
+          <MaterialIcons iconName="arrow_back" /> Back to search{' '}
+        </a>
+      </Link>
+      <p>path: {id}</p>
+    </JobDetailsContext.Provider>
+  );
 }
 
 export default Details;
