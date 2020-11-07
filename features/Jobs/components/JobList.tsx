fd70740360bd6c8f3df89da4a9b4bac6f748af2a
hare <hendrasadewa30@gmail.com>
2020-11-07 14:14:04 +0700

    add div to loading on joblist

--- features/Jobs/components/JobList.tsx
+++ features/Jobs/components/JobList.tsx
@@ -19,7 +19,13 @@ const JobList: React.FC = () => {
   }
 
   if (isLoading) {
-    return [...Array(10)].map(() => <JobCard isLoading={isLoading} />);
+    return (
+      <div>
+        {[...Array(10)].map(() => (
+          <JobCard isLoading={isLoading} />
+        ))}
+      </div>
+    );
   }
 
   if (ids && ids.length > 0) {
