4593edfe473653bf9537abad906c2023d7fcf6b7
hare <hendrasadewa30@gmail.com>
2020-11-07 17:05:45 +0700

    create and implement SEO component

---
+++ components/SEO.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import { FC } from 'react';
+import meta from '../public/json/meta.json';
+
+type propTypes = {
+  title: string;
+};
+
+const SEO: FC<propTypes> = ({ title }) => (
+  <Head>
+    <title>{title}</title>
+    {Object.keys(meta).map((key) => (
+      <meta name={key} content={meta[key]} key={key} />
+    ))}
+  </Head>
+);
+
+export default SEO;
